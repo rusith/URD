8d3257e1e54d9a029731cd73e5f1c5da6b0025bf
Shanaka rusith <srlsooriyagoda@gmail.com>
2016-01-01 14:34:07 +0530

    wrong comments
    removed wrong summary comments ,  added summary to GetCopyOfAObject method

--- UndoRedo/URD/Tools/SerializationTools.cs
+++ UndoRedo/URD/Tools/SerializationTools.cs
@@ -11,19 +11,14 @@ namespace URD.Tools
         /// serialize an object 
         /// </summary>
         /// <param name="obj">object to serialize</param>
-        /// <param name="encrypt">true=return text will encrypted, false=returns normal text</param>
-        /// <returns></returns>
+        /// <returns>returns serialized xml string</returns>
         public static string SerializeObject(object obj)
         {
-
             XmlSerializer xmlserializer = new XmlSerializer(obj.GetType());
-
             using (StringWriter writer = new StringWriter())
             {
                 xmlserializer.Serialize(writer, obj);
-
                 return writer.ToString();
-
             }
         }
 
@@ -32,20 +27,21 @@ namespace URD.Tools
         /// </summary>
         /// <param name="typeofobject">type of the object</param>
         /// <param name="str">string to deserialize</param>
-        /// <param name="decrypt">is string encrypted</param>
-        /// <returns></returns>
+        /// <returns>returns deserialized object</returns>
         public static object DeserializeString(Type type, string str)
         {
-
             XmlSerializer xmlserializer = new XmlSerializer(type);
             using (TextReader reader = new StringReader(str))
             {
-                object deserialized = xmlserializer.Deserialize(reader);
-
-                return deserialized;
+                return  xmlserializer.Deserialize(reader);
             }
         }
 
+        /// <summary>
+        /// get deep copy of a object using serialization mathod [serialize-->deserialize-->return]
+        /// </summary>
+        /// <param obj>object for copy</param>
+        /// <returns>copyed object</returns>
         public static object GetCopyOfAObject(object obj)
         {
             if (obj == null) return null;
