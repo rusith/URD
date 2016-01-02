958c96265ddb9bc9f0f8d818fa6f7497e6c2f07c
Shanaka rusith <srlsooriyagoda@gmail.com>
2016-01-01 22:43:29 -0800

    little change

--- UndoRedo/URD/Change.cs
+++ UndoRedo/URD/Change.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// this is the base class of all type of changes 
-    /// if you creating a new type of change class you must extend this class
+    /// if you creating a new type of change class you ( must ) extend this class
     /// </summary>
     public class Change
     {
